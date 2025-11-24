acc0fadae2e95c8ec285cb457c43007b17ce8443
salma hamdi <saalmahm51@gmail.com>
2025-11-24 23:56:43 +0100

    SA-11: Ajout du UserRepository

---
+++ src/main/java/ma/smartshop/smartshop/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ma.smartshop.smartshop.repository;
+
+import ma.smartshop.smartshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
