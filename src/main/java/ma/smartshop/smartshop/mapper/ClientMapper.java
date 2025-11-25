2b151a6702dd1838445c2cd93c4377470d8c675c
salma hamdi <saalmahm51@gmail.com>
2025-11-25 14:53:03 +0100

    SA-19: Ajout du mapper MapStruct pour la conversion entre Client et DTOs

---
+++ src/main/java/ma/smartshop/smartshop/mapper/ClientMapper.java
@@ -0,0 +1,17 @@
+package ma.smartshop.smartshop.client.mapper;
+
+import ma.smartshop.smartshop.client.dto.ClientRequestDto;
+import ma.smartshop.smartshop.client.dto.ClientResponseDto;
+import ma.smartshop.smartshop.entity.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface ClientMapper {
+
+    Client toEntity(ClientRequestDto dto);
+
+    ClientResponseDto toResponseDto(Client entity);
+
+    void updateEntityFromDto(ClientRequestDto dto, @MappingTarget Client entity);
+}
