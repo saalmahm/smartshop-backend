06781d92ef85854ed45ed209b894048745fce6b6
salma hamdi <saalmahm51@gmail.com>
2025-11-26 15:51:40 +0100

    SA-51: Ajout des méthodes paginées de recherche de produits (avec filtre sur le nom)

--- src/main/java/ma/smartshop/smartshop/repository/ProductRepository.java
+++ src/main/java/ma/smartshop/smartshop/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package ma.smartshop.smartshop.repository;
 
 import ma.smartshop.smartshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByDeletedFalse();
+    Page<Product> findByDeletedFalse(Pageable pageable);
+    Page<Product> findByDeletedFalseAndNameContainingIgnoreCase(String name, Pageable pageable);
 }
