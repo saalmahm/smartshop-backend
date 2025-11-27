bc7c197423c85b1d75aac21065ceef60d97236b9
salma hamdi <saalmahm51@gmail.com>
2025-11-27 12:09:38 +0100

    SA-77: Création de DTO PaymentCreateRequestDto

---
+++ src/main/java/ma/smartshop/smartshop/payment/dto/PaymentCreateRequestDto.java
@@ -0,0 +1,19 @@
+package ma.smartshop.smartshop.payment.dto;
+
+import lombok.Data;
+import ma.smartshop.smartshop.enums.PaymentType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class PaymentCreateRequestDto {
+
+    private Long orderId;
+    private BigDecimal amount;
+    private PaymentType type;
+    private LocalDate paymentDate;
+    private LocalDate dueDate;
+    private String reference;
+    private String bank;
+}
