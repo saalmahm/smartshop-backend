0c17cad045e80360ab14c0c6f577c7a77e21d10d
salma hamdi <saalmahm51@gmail.com>
2025-12-03 21:20:12 +0100

    Order: Requêtes supplémentaires pour historique client et promo unique

--- src/main/java/ma/smartshop/smartshop/repository/OrderRepository.java
+++ src/main/java/ma/smartshop/smartshop/repository/OrderRepository.java
@@ -2,6 +2,13 @@ package ma.smartshop.smartshop.repository;
 
 import ma.smartshop.smartshop.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ma.smartshop.smartshop.entity.Client;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByClientOrderByCreatedAtDesc(Client client);
+    boolean existsByPromoCode(String promoCode);
+
+
 }
