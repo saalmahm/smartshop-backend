5a37f024ec0df985c17b00102e3eeadcf37fc49c
salma hamdi <saalmahm51@gmail.com>
2025-12-03 21:19:31 +0100

    Client: Bean Validation sur les champs du DTO client

--- src/main/java/ma/smartshop/smartshop/dto/client/ClientRequestDto.java
+++ src/main/java/ma/smartshop/smartshop/dto/client/ClientRequestDto.java
@@ -1,11 +1,22 @@
 package ma.smartshop.smartshop.client.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 import lombok.Data;
 
 @Data
 public class ClientRequestDto {
+
+    @NotBlank(message = "Client name is required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Phone is required")
     private String phone;
+
+    @NotBlank(message = "Address is required")
     private String address;
 }
