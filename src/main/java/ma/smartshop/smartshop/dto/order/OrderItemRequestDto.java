49da02921e7ca70f5f1ca30ad5296c2e2aa23d65
salma hamdi <saalmahm51@gmail.com>
2025-12-03 21:19:51 +0100

    Order: Bean Validation sur les lignes de commande

--- src/main/java/ma/smartshop/smartshop/dto/order/OrderItemRequestDto.java
+++ src/main/java/ma/smartshop/smartshop/dto/order/OrderItemRequestDto.java
@@ -1,9 +1,16 @@
 package ma.smartshop.smartshop.dto.order;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 
 import lombok.Data;
 
 @Data
 public class OrderItemRequestDto {
+
+    @NotNull(message = "Product id is required")
     private Long productId;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
     private Integer quantity;
 }
